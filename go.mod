140f2322bb5fc13551e8f2575d3fcec9ca5c194a
themitchnz <jamesmitch@gmail.com>
2022-06-30 11:38:59 +1200

    Hi

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/themitchnz/wee-dude-machine
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	println("Hello, world!")
+}
+
+type WeeMan struct {
+	ram [100]int16
+	pc  int16
+}
